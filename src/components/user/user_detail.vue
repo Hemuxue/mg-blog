cd435247946441e05209fa807cc1f8fcdd74e0ae
hmx <527211775@qq.com>
2019-04-24 20:34:47 +0800

    feat: create user-detail page and init proxyTabel config

--- src/components/user/user_detail.vue
+++ src/components/user/user_detail.vue
@@ -1,13 +1,202 @@
 <template>
   <div class="user">
     user
+    <div class="user-box">
+      <a-form
+        :form="Registerform"
+        @submit="RegisterSubmit"
+      >
+        <a-form-item
+          v-bind="formItemLayout"
+          label="E-mail"
+        >
+          <a-input
+            :disabled="disableFlag"
+            v-decorator="[
+              'email',
+              {
+                rules: [{
+                  type: 'email', message: 'The input is not valid E-mail!',
+                }, {
+                  required: true, message: 'Please input your E-mail!',
+                }]
+              }
+            ]"
+          />
+        </a-form-item>
+        <a-form-item
+          v-bind="formItemLayout"
+          label="Password"
+        >
+          <a-input
+            v-decorator="[
+              'password',
+              {
+                rules: [{
+                  required: true, message: 'Please input your password!',
+                }, {
+                  validator: validateToNextPassword,
+                }],
+              }
+            ]"
+            type="password"
+            :disabled="disableFlag"
+          />
+        </a-form-item>
+        <a-form-item
+          v-bind="formItemLayout"
+          label="Confirm Password"
+        >
+          <a-input
+            v-decorator="[
+              'confirm',
+              {
+                rules: [{
+                  required: true, message: 'Please confirm your password!',
+                }, {
+                  validator: compareToFirstPassword,
+                }],
+              }
+            ]"
+            type="password"
+            @blur="handleConfirmBlur"
+            :disabled="disableFlag"
+          />
+        </a-form-item>
+        <a-form-item
+          v-bind="formItemLayout"
+          label="NickName"
+        >
+          <a-input
+            v-decorator="[
+              'nickname',
+              {
+                rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }]
+              },
+            ]"
+            :disabled="disableFlag"
+          />
+        </a-form-item>
+        <a-form-item
+          v-bind="formItemLayout"
+          label="Phone Number"
+        >
+          <a-input
+            v-decorator="[
+              'phone',
+              {
+                rules: [{ required: true, message: 'Please input your phone number!' }],
+              }
+            ]"
+            style="width: 100%"
+            :disabled="disableFlag"
+          >
+          </a-input>
+        </a-form-item>
+        <a-form-item v-bind="tailFormItemLayout">
+          <a-button
+            type="primary"
+            v-if="disableFlag"
+            @click.native="disableFlag = false"
+          >
+            编辑
+          </a-button>
+          <a-button
+            type="primary"
+            html-type="submit"
+            v-if="!disableFlag"
+          >
+            保存
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </div>
   </div>
 </template>
 <script>
 export default {
-
+  data(){
+    return {
+      formItemLayout: {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 8 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+      },
+      tailFormItemLayout: {
+        wrapperCol: {
+          xs: {
+            span: 24,
+            offset: 0,
+          },
+          sm: {
+            span: 16,
+            offset: 8,
+          },
+        },
+      },
+      disableFlag:true
+    }
+  },
+  beforeCreate () {
+    this.Registerform = this.$form.createForm(this);
+  },
+  methods: {
+    RegisterSubmit  (e) {
+      e.preventDefault();
+      console.log(this.Registerform)
+      this.Registerform.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+      // this.Registerform.setFieldsValue({
+      //   email:'12321321@qq.com'
+      // })
+    },
+    handleConfirmBlur  (e) {
+      console.log('触发')
+      console.log(e.target)
+      const value = e.target.value;
+      console.log(value)
+      this.confirmDirty = this.confirmDirty || !!value;
+    },
+    compareToFirstPassword  (rule, value, callback) {
+      console.log(rule, value, callback)
+      const form = this.Registerform;
+      console.log(value,form.getFieldValue('password'))
+      if (value && value !== form.getFieldValue('password')) {
+        callback('Two passwords that you enter is inconsistent!');
+      } else {
+        callback();
+      }
+    },
+    validateToNextPassword  (rule, value, callback) {
+      const form = this.Registerform;
+      if (value && this.confirmDirty) {
+        form.validateFields(['confirm'], { force: true });
+      }
+      callback();
+    },
+    callback (key) {
+      console.log(key)
+      if(key == 2){
+        this.tabKey = true
+      }else {
+        this.tabKey = false
+      }
+    },
+  }
 }
 </script>
 <style lang="less">
-
+  .user{
+    .user-box{
+      width:500px;
+    }
+  }
 </style>
--- src/components/user/user_list.vue
+++ src/components/user/user_list.vue
@@ -1,13 +1,114 @@
 <template>
-  <div class="user-list">
-    user-list
+  <div class="article-list">
+    <div class="search" style="maxWidth:300px">
+      <el-input
+        placeholder="按手机号/email查询"
+        v-model="search"
+        clearable>
+      </el-input>
+
+    </div>
+    <el-table
+      :stripe="true"
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        label='序号'
+        type="index"
+        align="center"
+        width="50"
+        :index="indexMethod">
+      </el-table-column>
+      <el-table-column
+        label="用户名"
+        width="180">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="手机号码"
+        width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-menu"></i>
+          <span style="margin-left: 10px">{{ scope.row.phone }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="邮箱"
+        width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-menu"></i>
+          <span style="margin-left: 10px">{{ scope.row.email }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="日期"
+        width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
 export default {
-
+  data() {
+    return {
+      search:'',
+      tableData: [
+        {
+          name: '深入理解js',
+          phone: '分类一',
+          email: '标签一',
+          date: '2016-05-02',
+        }, {
+          name: '深入理解js',
+          phone: '分类一',
+          email: '标签一',
+          date: '2016-05-02',
+        }, {
+          name: '深入理解js',
+          phone: '分类一',
+          email: '标签一',
+          date: '2016-05-02',
+        }, {
+          name: '深入理解js',
+          phone: '分类一',
+          email: '标签一',
+          date: '2016-05-02',
+        }
+      ]
+    }
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+    indexMethod(index) {
+      return index;
+    },
+  }
 }
 </script>
 <style lang="less">
 
 </style>
+
+
